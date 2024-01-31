0ea2d8d634256b8bab25f95f41ee2608be34207d
Ravinder010 <sin14142@sheridancollege.ca>
2024-01-30 19:43:05 -0500

    version 1 ICE1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -11,6 +11,8 @@ import java.util.Random;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * Modifier : Ravinder Singh
+ * Student ID : 991715910
  */
 public class CardTrick {
     
